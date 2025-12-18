61f6a636a17579982c78cbc77e26fe8dcfd530c6
GIT-gezzy <jdavichomaldonado@gmail.com>
2025-12-18 10:56:55 -0500

    Problema02 - paquete02

--- Taller10/src/paquete2/Problema02.java
+++ Taller10/src/paquete2/Problema02.java
@@ -10,5 +10,32 @@ package paquete2;
  * @author reroes
  */
 public class Problema02 {
-    
+
+    public static void main(String[] args) {
+        int[][] edades = {
+            {5, 12, 35, 40},
+            {8, 15, 33, 38},
+            {6, 10, 30, 36}};
+        int[] familias = new int[3];
+        int adultos;
+        int menores;
+        int i;
+        int j;
+        String reporte = "";
+        for (i = 0; i < familias.length; i++) {
+            adultos = 0;
+            menores = 0;
+            for (j = 0; j < 4; j++) {
+                if (edades[i][j] >= 18) {
+                    adultos = adultos + 1;
+                } else {
+                    menores = menores + 1;
+                }
+            }
+            familias[i] = adultos;
+            reporte = String.format("%sFamilia %s, tiene un total de %s adultos\n",
+                    reporte, i + 1, familias[i]);
+        }
+        System.out.printf("%s\n", reporte);
+    }
 }
