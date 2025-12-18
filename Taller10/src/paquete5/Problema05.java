b69d1764695ebe5079a3662f2067d85317f01d48
GIT-gezzy <jdavichomaldonado@gmail.com>
2025-12-18 12:31:40 -0500

    Problema05 - paquete05

--- Taller10/src/paquete5/Problema05.java
+++ Taller10/src/paquete5/Problema05.java
@@ -10,5 +10,24 @@ package paquete5;
  * @author reroes
  */
 public class Problema05 {
-    
+
+    public static void main(String[] args) {
+        String[][] estudiantes = {{"Sarah Ward", "Philip Payne"},
+        {"Carrie Burton", "Lauren Rice"},
+        {"Emma Escobar", "Lori Flores"},
+        {"Steven West", "Toni Martin"}};
+        int i;
+        int j;
+        String reporte = "";
+        String letra;
+        for (i = 0; i < estudiantes.length; i++) {
+            for (j = 0; j < 2; j++) {
+                letra = estudiantes[i][j].substring(0, 1);
+                if (letra.equals("S") || letra.equals("P") || letra.equals("T")) {
+                    reporte = String.format("%s%s\n", reporte, estudiantes[i][j]);
+                }
+            }
+        }
+        System.out.printf("%s", reporte);
+    }
 }
