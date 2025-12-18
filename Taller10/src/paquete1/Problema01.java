b58af70a840e7376d43b41c71c38c6920dd48cf4
GIT-gezzy <jdavichomaldonado@gmail.com>
2025-12-18 10:41:26 -0500

    Problema01 - paquete01

--- Taller10/src/paquete1/Problema01.java
+++ Taller10/src/paquete1/Problema01.java
@@ -14,7 +14,35 @@ public class Problema01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        boolean[][] asistencia = {
+            {true, true, false, true, true},
+            {true, false, false, true, false},
+            {true, true, true, true, true},
+            {false, true, true, false, true}};
+        String[] estudiantes = {"Ana", "Luis", "Carlos", "María"};
+        int dias[] = new int[5];
+        int i;
+        int j;
+        int contador;
+        String reporte = "";
+        for (i = 0; i < asistencia.length; i++) {
+            contador = 0;
+            for (j = 0; j < 5; j++) {
+                if (asistencia[i][j] == true) {
+                    contador = contador + 1;
+                }
+                dias[i] = contador;
+            }
+            if (contador == 5) {
+                reporte = String.format("%sEstudiante %s ha asistido un "
+                        + "total de %s dias (Asistencia Completa)\n",
+                        reporte, estudiantes[i], dias[i]);
+            } else {
+                reporte = String.format("%sEstudiante %s ha asistido un "
+                        + "total de %s dias\n",
+                        reporte, estudiantes[i], dias[i]);
+            }
+        }
+        System.out.printf("%s\n", reporte);
     }
-    
 }
